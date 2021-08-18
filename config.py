2008efe333821e8159128cf5322186e47a0cc3dc
lastlines <87518118+lastlines@users.noreply.github.com>
2021-08-18 08:46:36 +0700

    Update config.py

--- config.py
+++ config.py
@@ -51,12 +51,8 @@ class Config:
     ADMIN = os.environ.get("ADMINS", "")
     ADMINS = [int(admin) if re.search('^\d+$', admin) else admin for admin in (ADMIN).split()]
     API_ID = int(os.environ.get("API_ID", ""))
-    CHAT = int(os.environ.get("CHAT", ""))
-    LOG_GROUP=os.environ.get("LOG_GROUP", "")
-    if LOG_GROUP:
-        LOG_GROUP=int(LOG_GROUP)
-    else:
-        LOG_GROUP=None
+    CHAT = int(os.environ.get("CHAT", None))
+    LOG_GROUP=os.environ.get("LOG_GROUP", None)
     STREAM_URL=finalurl
     ADMIN_ONLY=os.environ.get("ADMIN_ONLY", "False")
     REPLY_MESSAGE=os.environ.get("REPLY_MESSAGE", None)
